9307f6def4797d1de7bfb5a6902f90ca64c7e9e2
Reid Prichard <reideprichard@gmail.com>
2024-03-01 13:22:54 -0500

    Reimplimenting mesh read loop

--- src/lib.rs
+++ src/lib.rs
@@ -55,88 +55,19 @@ pub mod mesh {
         LeastSquares,
     }
 
-    // struct VectorGradient {
-    //
-    // }
-
-    pub trait Cell {
-        fn calculate_pressure_gradient(
-            &mut self,
-            reconstruction_method: GradientReconstructionMethods,
-        );
-        // Might be cleaner to just have one get() and one set() with u,v,w,p enum values
-        fn get_u(&self) -> Float;
-        fn get_v(&self) -> Float;
-        fn get_w(&self) -> Float;
-        fn get_p(&self) -> Float;
-        fn set_u(&mut self, u: Float);
-        fn set_v(&mut self, v: Float);
-        fn set_w(&mut self, w: Float);
-        fn set_p(&mut self, p: Float);
+    pub struct Cell {
+        pub face_indices: Vec<Uint>,
+        pub node_indices: Vec<Uint>,
+        pub centroid: Vector,
+        pub velocity: Vector,
+        pub pressure: Float,
     }
-
-    macro_rules! cell_type {
-        ($name: ident, $node_count: literal, $face_count: literal) => {
-            struct $name {
-                pub nodes: [&'static Node; $node_count],
-                pub faces: [&'static Face; $face_count],
-                pub centroid: Vector,
-                pub velocity: Vector,
-                pub pressure: Float,
-                pub pressure_gradient: Vector,
-            }
-
-            impl Cell for $name {
-                fn get_u(&self) -> Float { self.velocity.x }
-                fn get_v(&self) -> Float { self.velocity.y }
-                fn get_w(&self) -> Float { self.velocity.z }
-                fn get_p(&self) -> Float { self.pressure }
-                fn set_u(&mut self, u: Float) {self.velocity.x = u; }
-                fn set_v(&mut self, v: Float) {self.velocity.y = v; }
-                fn set_w(&mut self, w: Float) {self.velocity.z = w; }
-                fn set_p(&mut self, p: Float) {self.pressure = p; }
-                fn calculate_pressure_gradient(
-                    &mut self,
-                    reconstruction_method: GradientReconstructionMethods,
-                ) {
-                    use GradientReconstructionMethods::*;
-                    use GreenGaussVariants::*;
-                    match reconstruction_method {
-                        GreenGauss(variant) => {
-                            // 1. Iterate over faces in cell
-                            // 2. For each face, calculate face value
-                            // 3. Perform Green Gauss
-                            match variant {
-                                CellBased => {}
-                                NodeBased => {}
-                            }
-                            self.pressure_gradient = Vector {
-                                x: 0 as Float,
-                                y: 0 as Float,
-                                z: 0 as Float,
-                            };
-                        }
-                        _ => {}
-                    }
-                    // iterate over faces and interpolate face values
-                    // perform green gauss
-                    //
-                }
-            }
-        };
-    }
-
-    cell_type!(Tet, 4, 4);
-    cell_type!(Pyramid, 5, 5);
-    cell_type!(Prism, 6, 5);
-    cell_type!(Hex, 8, 6);
-    cell_type!(Poly, 0, 0); // Unimplemented
 
     pub struct Node {
-        cells: Vec<Box<dyn Cell>>,
-        position: Vector,
-        pressure: Float,
-        velocity: Vector,
+        pub cell_indices: Vec<Uint>,
+        pub position: Vector,
+        pub velocity: Vector,
+        pub pressure: Float,
     }
     impl Node {
         fn interpolate_velocity(&mut self) {}
@@ -149,17 +80,20 @@ pub mod mesh {
     }
 
     pub struct Face {
-        cells: Vec<Box<dyn Cell>>,
-        centroid: Vector,
-        pressure: Float,
-        velocity: Vector,
+        pub cell_indices: Vec<Uint>,
+        pub node_indices: Vec<Uint>,
+        pub centroid: Vector,
+        pub pressure: Float,
+        pub velocity: Vector,
     }
     impl Face {
     }
 
     pub struct Mesh {
-        nodes: Vec<Node>,
-        faces: Vec<Face>,
-        cells: Vec<Box<dyn Cell>>,
+        pub nodes: Vec<Node>,
+        pub faces: Vec<Face>,
+        pub cells: Vec<Cell>,
+    }
+    impl Mesh {
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,55 +2,251 @@
 #![allow(unused_imports)]
 #![allow(unused_variables)]
 
-pub mod io {
+macro_rules! skip_zone_zero {
+    ($label:tt, $line_blocks:expr) => {
+        if $line_blocks[1] == "(0" {
+            break $label;
+        }
+    };
+}
+
+macro_rules! end_section {
+    ($label:tt, $parser_state:expr) => {{
+        $parser_state = MeshReadState::Parsing;
+        break $label;
+    }};
+}
+
+pub mod solver_io {
     use orc::common::*;
     use orc::mesh;
+    use orc::mesh::Cell;
+    use orc::mesh::Face;
+    use orc::mesh::Mesh;
+    use orc::mesh::Node;
     use std::fs;
+    use std::fs::File;
+    use std::io::{self, BufRead};
 
-    pub fn read_mesh(mesh_path: &str, msh: &mut mesh::Mesh) {
-        macro_rules! skip_zone_zero {
-            ($line_blocks:expr) => {
-                if let "(0" = $line_blocks[1] {
-                    continue;
-                }
-            };
+    enum MeshReadState {
+        Parsing,
+        ReadingNodes,
+        ReadingFaces,
+        ReadingCells,
+    }
+
+    pub fn read_mesh(mesh_path: &str) -> mesh::Mesh {
+        fn read_mesh_lines(filename: &str) -> io::Result<io::Lines<io::BufReader<File>>> {
+            // Example had filename generic type P and ... where P: AsRef<Path> ... in signature but I
+            // don't understand it ¯\_(ツ)_/¯
+            let file = File::open(filename)?;
+            Ok(io::BufReader::new(file).lines())
         }
 
-        for line in fs::read_to_string(mesh_path)
-            .expect("mesh exists and is readable")
-            .lines()
-        {
-            // 99% sure mesh files are ASCII?
-            let line_blocks: Vec<&str> = line.split_ascii_whitespace().collect();
-            match line_blocks[0] {
-                "(0" => continue, // comment
-                "(1" => continue, // header
-                "(2" => (),       // dimensions
-                "(10" => {
-                    skip_zone_zero!(line_blocks);
-                    println!("Beginning reading nodes."); // nodes
+        let mut nodes: Vec<Node> = Vec::new();
+        let mut faces: Vec<Face> = Vec::new();
+        let mut cells: Vec<Cell> = Vec::new();
+
+        let mut node_indices: Vec<Uint> = Vec::new();
+        let mut face_indices: Vec<Uint> = Vec::new();
+        let mut cell_indices: Vec<Uint> = Vec::new();
+
+        // let mesh = Mesh {
+        //     nodes,
+        //     faces,
+        //     cells,
+        // };
+
+        let mut state = MeshReadState::Parsing;
+        let mut start_index: Uint = 0;
+        let mut end_index: Uint = 0;
+
+        struct CellType {
+            id: i8,
+            node_count: i16,
+            face_count: i16,
+        }
+        let tet = CellType {
+            id: 2,
+            node_count: 4,
+            face_count: 4,
+        };
+        let hex = CellType {
+            id: 3,
+            node_count: 8,
+            face_count: 6,
+        };
+
+        if let Ok(mesh_file_lines) = read_mesh_lines(mesh_path) {
+            dbg!();
+            let mut lines = mesh_file_lines.flatten();
+            let mut section_header_line = lines.next().expect("mesh is at least one line long");
+            loop {
+                let section_header_blocks: Vec<&str> =
+                    section_header_line.split_ascii_whitespace().collect();
+                match section_header_blocks[0] {
+                    "(0" => (), // comment
+                    "(1" => (), // header
+                    "(2" => (),       // dimensions
+                    "(10" => 'read_nodes: {
+                        skip_zone_zero!('read_nodes, section_header_blocks);
+                        start_index = Uint::from_str_radix(section_header_blocks[2], 16)
+                            .expect("third value exists");
+                        end_index = Uint::from_str_radix(section_header_blocks[3], 16)
+                            .expect("fourth value exists");
+                        println!("Beginning reading nodes from {start_index} to {end_index}.");
+                        let mut current_line = lines.next().expect("section header has contents");
+                        'node_loop: loop {
+                            let line_blocks: Vec<&str> =
+                                current_line.split_ascii_whitespace().collect();
+                            if line_blocks.len() != 3 {
+                                end_section!('node_loop, state);
+                            }
+                            println!("{section_header_line}");
+                            let x = line_blocks[0].parse::<Float>();
+                            let y = line_blocks[1].parse::<Float>();
+                            let z = line_blocks[2].parse::<Float>();
+                            if x.is_ok() && y.is_ok() && z.is_ok() {
+                                nodes.push(Node {
+                                    cell_indices: Vec::new(),
+                                    position: Vector {
+                                        x: x.unwrap(),
+                                        y: y.unwrap(),
+                                        z: z.unwrap(),
+                                    },
+                                    velocity: Vector {
+                                        x: 0.,
+                                        y: 0.,
+                                        z: 0.,
+                                    },
+                                    pressure: 0.,
+                                });
+                                match lines.next() {
+                                    Some(line_contents) => current_line = line_contents,
+                                    None => end_section!('node_loop, state),
+                                }
+                                // current_line = match lines.next() {
+                                //     Ok(line) => line,
+                                //     Err(e) => e;
+                                // }
+                            } else {
+                                end_section!('node_loop, state);
+                            }
+                        }
+                    }
+                    "(18" => {
+                        // skip_zone_zero!(section_header_blocks);
+                        println!("Beginning reading shadow faces."); // periodic shadow faces
+                    }
+                    "(12" => 'read_cells: {
+                        skip_zone_zero!('read_cells, section_header_blocks);
+                        start_index = Uint::from_str_radix(section_header_blocks[2], 16)
+                            .expect("third value exists");
+                        end_index = Uint::from_str_radix(section_header_blocks[3], 16)
+                            .expect("fourth value exists");
+                        println!("Beginning reading cells."); // cells
+                    }
+                    "(13" => 'read_faces: {
+                        skip_zone_zero!('read_faces, section_header_blocks);
+                        start_index = Uint::from_str_radix(section_header_blocks[2], 16)
+                            .expect("third value exists");
+                        end_index = Uint::from_str_radix(section_header_blocks[3], 16)
+                            .expect("fourth value exists");
+                        println!("Beginning reading faces."); // faces
+                    }
+                    "(58" => (), // cell tree
+                    "(59" => (), // face tree
+                    "(61" => (), // interface face parents for nonconformal
+                    _ => (),
                 }
-                "(18" => {
-                    skip_zone_zero!(line_blocks);
-                    println!("Beginning reading shadow faces."); // periodic shadow faces
-                }
-                "(12" => {
-                    skip_zone_zero!(line_blocks);
-                    println!("Beginning reading cells."); // cells
-                }
-                "(13" => {
-                    skip_zone_zero!(line_blocks);
-                    println!("Beginning reading faces."); // faces
-                }
-                "(58" => continue, // cell tree
-                "(59" => continue, // face tree
-                "(61" => continue, // interface face parents for nonconformal
-                _ => continue,
+                section_header_line = lines.next().expect("mesh is at least one line long");
+                println!("{section_header_line}")
             }
-            // line.trim_start_matches("(0")
-            println!("Line: '{line}'");
         }
-        // println!("Mesh contents:\n{mesh_contents}");
+        // for line in read_mesh_lines(mesh_path).unwrap().flatten() {
+        //     // 99% sure mesh files are ASCII?
+        //     let line_blocks: Vec<&str> = line.split_ascii_whitespace().collect();
+        //     match state {
+        //         MeshReadState::Parsing => {
+        //             match line_blocks[0] {
+        //                 "(0" => continue, // comment
+        //                 "(1" => continue, // header
+        //                 "(2" => (),       // dimensions
+        //                 "(10" => {
+        //                     skip_zone_zero!(line_blocks);
+        //                     state = MeshReadState::ReadingNodes;
+        //                     start_index = Uint::from_str_radix(line_blocks[2], 16)
+        //                         .expect("third value exists");
+        //                     end_index = Uint::from_str_radix(line_blocks[3], 16)
+        //                         .expect("fourth value exists");
+        //                     println!("Beginning reading nodes from {start_index} to {end_index}.");
+        //                     // nodes
+        //                 }
+        //                 "(18" => {
+        //                     skip_zone_zero!(line_blocks);
+        //                     println!("Beginning reading shadow faces."); // periodic shadow faces
+        //                 }
+        //                 "(12" => {
+        //                     skip_zone_zero!(line_blocks);
+        //                     state = MeshReadState::ReadingCells;
+        //                     start_index = Uint::from_str_radix(line_blocks[2], 16)
+        //                         .expect("third value exists");
+        //                     end_index = Uint::from_str_radix(line_blocks[3], 16)
+        //                         .expect("fourth value exists");
+        //                     println!("Beginning reading cells."); // cells
+        //                 }
+        //                 "(13" => {
+        //                     skip_zone_zero!(line_blocks);
+        //                     state = MeshReadState::ReadingFaces;
+        //                     start_index = Uint::from_str_radix(line_blocks[2], 16)
+        //                         .expect("third value exists");
+        //                     end_index = Uint::from_str_radix(line_blocks[3], 16)
+        //                         .expect("fourth value exists");
+        //                     println!("Beginning reading faces."); // faces
+        //                 }
+        //                 "(58" => continue, // cell tree
+        //                 "(59" => continue, // face tree
+        //                 "(61" => continue, // interface face parents for nonconformal
+        //                 _ => continue,
+        //             }
+        //         }
+        //         MeshReadState::ReadingNodes => {
+        //             if line_blocks.len() != 3 {
+        //                 continue;
+        //             }
+        //             println!("{line}");
+        //             let x = line_blocks[0].parse::<Float>();
+        //             let y = line_blocks[1].parse::<Float>();
+        //             let z = line_blocks[2].parse::<Float>();
+        //             if x.is_ok() && y.is_ok() && z.is_ok() {
+        //                 nodes.push(Node {
+        //                     cell_indices: Vec::new(),
+        //                     position: Vector {
+        //                         x: x.unwrap(),
+        //                         y: y.unwrap(),
+        //                         z: z.unwrap(),
+        //                     },
+        //                     velocity: Vector {
+        //                         x: 0.,
+        //                         y: 0.,
+        //                         z: 0.,
+        //                     },
+        //                     pressure: 0.,
+        //                 });
+        //             } else {
+        //                 // todo: read this line on the next pass
+        //                 state = MeshReadState::Parsing;
+        //             }
+        //         }
+        //         MeshReadState::ReadingFaces => {}
+        //         MeshReadState::ReadingCells => {}
+        //     }
+        // }
+        Mesh {
+            nodes,
+            faces,
+            cells,
+        }
     }
 
     pub fn read_data() {}
@@ -93,6 +289,10 @@ fn main() {
     // 6. Write settings
     // 7. Run solver
     println!("Hello, world!");
-    let msh = mesh::Mesh{};
-    io::read_mesh("/home/admin/3x3_cube.msh", msh);
+    // let msh = mesh::Mesh{
+    //     nodes: Vec<mesh::Node>,
+    //     faces: Vec<mesh::Face>,
+    //     cells: Vec<mesh::Cell>,
+    // };
+    solver_io::read_mesh("/home/admin/3x3_cube.msh");
 }
