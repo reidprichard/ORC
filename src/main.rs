6f0040502d656c5813295e168ad01355fceeed27
Reid Prichard <reideprichard@gmail.com>
2024-03-15 18:35:22 -0400

    check test_3d mesh dimensions

--- src/main.rs
+++ src/main.rs
@@ -14,13 +14,16 @@ fn test_2d() -> Mesh {
     let face_min = f32::min(cell_width, cell_height);
     let face_max = f32::max(cell_width, cell_height);
     let mut mesh = orc::io::read_mesh("./examples/2D_3x6.msh");
+
     mesh.get_face_zone("INLET").zone_type = BoundaryConditionTypes::PressureInlet;
     mesh.get_face_zone("OUTLET").zone_type = BoundaryConditionTypes::PressureOutlet;
     mesh.get_face_zone("BOTTOM").zone_type = BoundaryConditionTypes::Wall;
     mesh.get_face_zone("TOP").zone_type = BoundaryConditionTypes::Wall;
-    let (face_min_actual, face_max_actual) = mesh.faces.iter().fold((face_max, face_min), |acc, (i, f)| {
-        (f32::min(acc.0, f.area), f32::max(acc.1, f.area))
-    });
+
+    let (face_min_actual, face_max_actual) =
+        mesh.faces.iter().fold((face_max, face_min), |acc, (i, f)| {
+            (f32::min(acc.0, f.area), f32::max(acc.1, f.area))
+        });
     let area_tolerance = 0.001;
     if (face_min_actual + area_tolerance < face_min) {
         panic!("face calculated as too small");
@@ -41,11 +44,36 @@ fn test_2d() -> Mesh {
 }
 
 fn test_3d() -> Mesh {
+    let cell_size = 1. / 3.;
+    let face_area = cell_size * cell_size;
+    let cell_volume = face_area * cell_size;
     let mut mesh = orc::io::read_mesh("./examples/3x3_cube.msh");
+
     mesh.get_face_zone("INLET").zone_type = BoundaryConditionTypes::PressureInlet;
     mesh.get_face_zone("OUTLET").zone_type = BoundaryConditionTypes::PressureOutlet;
     mesh.get_face_zone("PERIODIC_-Z").zone_type = BoundaryConditionTypes::Wall;
     mesh.get_face_zone("PERIODIC_+Z").zone_type = BoundaryConditionTypes::Wall;
+
+    let (face_min_actual, face_max_actual) =
+        mesh.faces.iter().fold((face_area, face_area), |acc, (i, f)| {
+            (f32::min(acc.0, f.area), f32::max(acc.1, f.area))
+        });
+    let area_tolerance = 0.001;
+    if (face_min_actual + area_tolerance < face_area) {
+        panic!("face calculated as too small");
+    }
+    if (face_max_actual - area_tolerance > face_area) {
+        panic!("face calculated as too large");
+    }
+    let cell_tolerance = 0.0001;
+    for (i, cell) in &mesh.cells {
+        if f32::abs(cell.volume - cell_volume) > cell_tolerance {
+            println!("Volume should be: {cell_volume}");
+            println!("Volume is: {}", cell.volume);
+            panic!("cell volume wrong");
+        }
+    }
+
     mesh
 }
 
